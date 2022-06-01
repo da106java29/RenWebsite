772f2b64bb6bd8eb95caf73d5f404b8692f3b016
Ren <da106java29@gmail.com>
2022-06-01 09:17:35 +0800

    test add new project

--- Serializable_Pratice/src/idv/ren/serializable/impl/Serializable_Impl.java
+++ Serializable_Pratice/src/idv/ren/serializable/impl/Serializable_Impl.java
@@ -15,6 +15,7 @@ public class Serializable_Impl {
 	
 	public static void main(String[] args) {
 		
+		
 		try (
 			FileOutputStream fos = new FileOutputStream(fileName);
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
