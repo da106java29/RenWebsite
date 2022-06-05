902f262e801b8335af0d215d1e37e7900f14ee90
Ren <da106java29@gmail.com>
2022-06-05 16:06:06 +0800

    Init The Spring Project

---
+++ RenWebsite/src/idv/ren/init_controller/WebController.java
@@ -0,0 +1,16 @@
+package idv.ren.init_controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WebController {
+	
+	@RequestMapping("/index")
+	public ModelAndView doIndex() {
+		ModelAndView view = new ModelAndView("index");
+		
+		return view;
+	}
+}
